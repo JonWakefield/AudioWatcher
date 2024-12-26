4ca46e4821b1d23f2368404c7ee3885cd9814923
Jon Wakefield <jonwakefield.mi@gmail.com>
2024-12-25 18:10:35 -0700

    Cleaned up log statements, updated version number

--- background.js
+++ background.js
@@ -7,35 +7,30 @@ let audioTabId; // type: str (maybe int idk)
 let tabsWithAudio; // type: Set 
 let whiteListTabs; // type: Set
 
-// 
 chrome.storage.local.get(["audioTabId"], async function (result) {
     audioTabId = result.audioTabId;
-    console.log("Current Audio Tab: ", audioTabId)
+    // console.log("Current Audio Tab: ", audioTabId)
 })
 
 
 chrome.storage.local.get(["whiteListTabs"], async function (result) {
     whiteListTabs = new Set(result.whiteListTabs);
-    console.log("WL tabs retrieved: ", whiteListTabs);
+    // console.log("WL tabs retrieved: ", whiteListTabs);
 })
 
 chrome.storage.local.get(["tabsWithAudio"], async function (result) {
     tabsWithAudio = new Set(result.tabsWithAudio);
-    console.log("Current tabs with audiio: ", tabsWithAudio);
+    // console.log("Current tabs with audiio: ", tabsWithAudio);
 })
 
 async function updateTab(newTabId) {
     audioTabId = newTabId;
     // update persistant storage
-    chrome.storage.local.set({ audioTabId: newTabId}).then(() => {
-        console.log("Updated audio tab id to ", newTabId);
-    })
+    chrome.storage.local.set({ audioTabId: newTabId})
 
     // reset tabs with audio
     tabsWithAudio = new Set(); // reset audio tabs
-    chrome.storage.local.set({ tabsWithAudio: Array.from(tabsWithAudio)}).then(() => {
-        console.log("Tabs with audio set: ", tabsWithAudio);
-    })
+    chrome.storage.local.set({ tabsWithAudio: Array.from(tabsWithAudio)})
 }
 
 function addWLTab(tabId) {
@@ -43,15 +38,11 @@ function addWLTab(tabId) {
     if (tabsWithAudio.has(tabId)) {
         checkIfEmpty(tabId);
     }
-    chrome.storage.local.set({ whiteListTabs: Array.from(whiteListTabs)}).then(() => {
-        console.log("white list tabs added: ", whiteListTabs)
-    })
+    chrome.storage.local.set({ whiteListTabs: Array.from(whiteListTabs)})
 }
 function removeWLTab(tabId) {
     whiteListTabs.delete(tabId);
-    chrome.storage.local.set({ whiteListTabs: Array.from(whiteListTabs)}).then(() => {
-        console.log("white list tabs added: ", whiteListTabs)
-    })
+    chrome.storage.local.set({ whiteListTabs: Array.from(whiteListTabs)})
 }
 
 function checkIfEmpty(tabId) {
@@ -59,9 +50,10 @@ function checkIfEmpty(tabId) {
     if (tabsWithAudio.size === 0) {
         toggleMuteState(audioTabId, false)
     }
-    chrome.storage.local.set({ tabsWithAudio: Array.from(tabsWithAudio)}).then(() => {
-        console.log("Tabs with audio set: ", tabsWithAudio);
-    })
+    // chrome.storage.local.set({ tabsWithAudio: Array.from(tabsWithAudio)}).then(() => {
+    //     console.log("Tabs with audio set: ", tabsWithAudio);
+    // })
+    chrome.storage.local.set({ tabsWithAudio: Array.from(tabsWithAudio)})
 }
 
 chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
@@ -89,9 +81,7 @@ chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
     if (tab.audible === true && tabId != audioTabId && !whiteListTabs.has(tabId)) {
         tabsWithAudio.add(tabId);
         toggleMuteState(audioTabId, true);
-        chrome.storage.local.set({tabsWithAudio: Array.from(tabsWithAudio)}).then(() => {
-            console.log("tabs with audio set: ", tabsWithAudio);
-        })
+        chrome.storage.local.set({tabsWithAudio: Array.from(tabsWithAudio)})
     }
     if (tab.audible === false && tabsWithAudio.has(tabId)) {
         checkIfEmpty(tabId)
@@ -104,7 +94,7 @@ chrome.tabs.onRemoved.addListener(function(tabId, removeInfo) {
             checkIfEmpty(tabId);
         }
     } catch (err) {
-        console.error("Error On tab Removal: ", err)
+        console.log("Error On tab Removal: ", err)
         tabsWithAudio = new Set();
     }
     if (whiteListTabs.has(tabId)) {
@@ -117,6 +107,6 @@ async function toggleMuteState(tabId, muted) {
     try {
         await chrome.tabs.update(tabId, {muted});
     } catch (err) {
-        console.error("Error: ", err);
+        console.log(err);
     }
 }
